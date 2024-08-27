10e3e66459acf6d489760a236bed31fc26aa56ba
Walleen.Ates <walleenates.202100119@gmail.com>
2024-08-27 14:46:53 +0800

    cap

--- src/App.js
+++ src/App.js
@@ -55,32 +55,7 @@ function App() {
           />
         </Routes>
       </Router>
-      <div className="manage-user">
-      <h2>Manage Users</h2>
-      <table className="user-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>
-                <button>Edit</button>
-                <button>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      
     </div>
   );
 }
